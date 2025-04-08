9da753314ab710e0b156de05f46bdcbf213c7231
syed_sitecore <XMCtest@sitecore.com>
2025-04-08 09:28:22 +0000

    Updated JSS version to 22.5.4

--- headapps/nextjs-starter/src/Scripts.tsx
+++ headapps/nextjs-starter/src/Scripts.tsx
@@ -1,5 +1,18 @@
-const Scripts = (): JSX.Element | null => {
-  return null;
+import { EditingScripts } from '@sitecore-jss/sitecore-jss-nextjs';
+// The BYOC bundle imports external (BYOC) components into the app and makes sure they are ready to be used
+import BYOC from 'src/byoc';
+import CdpPageView from 'components/CdpPageView';
+import FEAASScripts from 'components/FEAASScripts';
+
+const Scripts = (): JSX.Element => {
+  return (
+    <>
+      <BYOC />
+      <CdpPageView />
+      <FEAASScripts />
+      <EditingScripts />
+    </>
+  );
 };
 
 export default Scripts;
