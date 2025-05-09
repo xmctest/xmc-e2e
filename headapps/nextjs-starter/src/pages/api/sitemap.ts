c934f045620d17e24df66a9a8a72fa6f98cd9ffb
farh_sitecore <XMCtest@sitecore.com>
2025-05-09 08:32:06 +0000

    Updated JSS version to 0.2.0-canary.26

--- headapps/nextjs-starter/src/pages/api/sitemap.ts
+++
@@ -1,6 +0,0 @@
-import { SitemapMiddleware } from '@sitecore-content-sdk/nextjs/middleware';
-import scClient from 'lib/sitecore-client';
-
-const handler = new SitemapMiddleware(scClient).getHandler();
-
-export default handler;
