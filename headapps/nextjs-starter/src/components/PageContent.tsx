d476a98fb31d9cc6fed5760334c54ba5ab15cc2a
Syed Afiq Syed Itam <syedafiq.syeditam+sitecore@sitecore.com>
2025-06-30 18:18:31 +0800

    update to canary 31

---
+++ headapps/nextjs-starter/src/components/PageContent.tsx
@@ -0,0 +1,28 @@
+// @ts-nocheck
+
+import React, { JSX } from 'react';
+import { RichText as JssRichText, useSitecore, RichTextField } from '@sitecore-content-sdk/nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+interface Fields {
+  Content: RichTextField;
+}
+
+type PageContentProps = ComponentProps & {
+  fields: Fields;
+};
+
+export const Default = ({ params, fields }: PageContentProps): JSX.Element => {
+  const { pageContext } = useSitecore();
+  const { styles, RenderingIdentifier: id } = params;
+
+  const field = fields?.Content ?? (pageContext?.route?.fields?.Content as RichTextField);
+
+  return (
+    <div className={`component content ${styles}`} id={id}>
+      <div className="component-content">
+        <div className="field-content">{field ? <JssRichText field={field} /> : '[Content]'}</div>
+      </div>
+    </div>
+  );
+};
---
+++ headapps/nextjs-starter/src/components/PartialDesignDynamicPlaceholder.tsx
@@ -0,0 +1,11 @@
+// @ts-nocheck
+
+import React, { JSX } from 'react';
+import { Placeholder } from '@sitecore-content-sdk/nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+const PartialDesignDynamicPlaceholder = (props: ComponentProps): JSX.Element => (
+  <Placeholder name={props.rendering?.params?.sig || ''} rendering={props.rendering} />
+);
+
+export default PartialDesignDynamicPlaceholder;
