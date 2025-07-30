fb9f5f71b9526b1cd7e6bb25e98a590ebf37fe0e
farh_sitecore <XMCtest@sitecore.com>
2025-07-30 10:45:56 +0300

    Updated JSS version to 1.1.0-canary.11

--- headapps/nextjs-starter/src/Layout.tsx
+++ headapps/nextjs-starter/src/Layout.tsx
@@ -6,6 +6,7 @@ import Head from 'next/head';
 import { Placeholder, Field, DesignLibrary, Page } from '@sitecore-content-sdk/nextjs';
 import Scripts from 'src/Scripts';
 import SitecoreStyles from 'src/components/content-sdk/SitecoreStyles';
+import importMap from '.sitecore/import-map';
 
 interface LayoutProps {
   page: Page;
@@ -34,7 +35,7 @@ const Layout = ({ page }: LayoutProps): JSX.Element => {
       {/* root placeholder for the app, which we add components to using route data */}
       <div className={mainClassPageEditing}>
         {mode.isDesignLibrary ? (
-          <DesignLibrary />
+          <DesignLibrary importMap={importMap} />
         ) : (
           <>
             <header>
