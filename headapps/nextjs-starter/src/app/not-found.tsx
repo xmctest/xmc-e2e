b76c8ef4e355b9495ba202b8889e4b1874694fbd
xmctest <XMCtest@sitecore.com>
2025-10-14 21:05:34 +0800

    update sdk

--- headapps/nextjs-starter/src/app/not-found.tsx
+++ headapps/nextjs-starter/src/app/not-found.tsx
@@ -1,32 +1,24 @@
-import Link from "next/link";
-import client from "lib/sitecore-client";
-import scConfig from "sitecore.config";
-import { ErrorPage } from "@sitecore-content-sdk/nextjs";
-import Layout from "src/Layout";
-import Providers from "src/Providers";
+import Link from 'next/link';
+import client from 'lib/sitecore-client';
+import scConfig from 'sitecore.config';
+import { ErrorPage } from '@sitecore-content-sdk/nextjs';
+import Layout from 'src/Layout';
+import Providers from 'src/Providers';
 
 export default async function NotFound() {
-  if (!scConfig.defaultSite || scConfig.defaultSite.trim() === "") {
-    return (
-      <div style={{ padding: 10 }}>
-        <h1>Page not found</h1>
-        <p>This page does not exist.</p>
-        <Link href="/">Go to the Home page</Link>
-      </div>
-    );
-  }
+  if (scConfig.defaultSite) {
+    const page = await client.getErrorPage(ErrorPage.NotFound, {
+      site: scConfig.defaultSite,
+      locale: scConfig.defaultLanguage,
+    });
 
-  const page = await client.getErrorPage(ErrorPage.NotFound, {
-    site: scConfig.defaultSite,
-    locale: scConfig.defaultLanguage,
-  });
-
-  if (page) {
-    return (
-      <Providers page={page}>
-        <Layout page={page} />
-      </Providers>
-    );
+    if (page) {
+      return (
+        <Providers page={page}>
+          <Layout page={page} />
+        </Providers>
+      );
+    }
   }
 
   return (
