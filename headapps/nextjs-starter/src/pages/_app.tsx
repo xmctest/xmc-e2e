c7f7efa5d9f9813cb22c6194ee14ec1268ea4ece
farh_sitecore <XMCtest@sitecore.com>
2025-11-06 11:01:42 +0200

    Updated JSS version to 22.9.1

--- headapps/nextjs-starter/src/pages/_app.tsx
+++ headapps/nextjs-starter/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { I18nProvider } from 'next-localization';
 import { SitecorePageProps } from 'lib/page-props';
 import Bootstrap from 'src/Bootstrap';
 
-import 'assets/main.scss';
+import 'assets/app.css';
 
 function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element {
   const { dictionary, ...rest } = pageProps;
