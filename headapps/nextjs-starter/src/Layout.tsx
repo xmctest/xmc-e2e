1be9f2d9333f004e740068941aa8fa7246a30950
xmctest <xmctest@sitecore.com>
2025-11-06 16:36:17 +0800

    upgrade 22.10.0-canary.8

--- headapps/nextjs-starter/src/Layout.tsx
+++ headapps/nextjs-starter/src/Layout.tsx
@@ -1,12 +1,21 @@
+/**
+ * This Layout is needed for Starter Kit.
+ */
 import { JSX } from 'react';
 import Head from 'next/head';
-import { Placeholder, LayoutServiceData, Field, HTMLLink } from '@sitecore-jss/sitecore-jss-nextjs';
+import {
+  LayoutServiceData,
+  Field,
+  HTMLLink,
+  Placeholder,
+  DesignLibrary,
+  RenderingType,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 import config from 'temp/config';
-import Navigation from 'src/Navigation';
 import Scripts from 'src/Scripts';
 
-// Prefix public assets with a public URL to enable compatibility with Sitecore editors.
-// If you're not supporting Sitecore editors, you can remove this.
+// Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
+// If you're not supporting the Experience Editor, you can remove this.
 const publicUrl = config.publicUrl;
 
 interface LayoutProps {
@@ -16,28 +25,48 @@ interface LayoutProps {
 
 interface RouteFields {
   [key: string]: unknown;
-  pageTitle: Field;
+  Title?: Field;
 }
 
 const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
   const { route } = layoutData.sitecore;
-
   const fields = route?.fields as RouteFields;
+  const isPageEditing = layoutData.sitecore.context.pageEditing;
+  const mainClassPageEditing = isPageEditing ? 'editing-mode' : 'prod-mode';
+
+  const renderContent = () => (
+    <>
+      <header>
+        <div id="header">{route && <Placeholder name="headless-header" rendering={route} />}</div>
+      </header>
+      <main>
+        <div id="content">{route && <Placeholder name="headless-main" rendering={route} />}</div>
+      </main>
+      <footer>
+        <div id="footer">{route && <Placeholder name="headless-footer" rendering={route} />}</div>
+      </footer>
+    </>
+  );
 
   return (
     <>
       <Scripts />
       <Head>
-        <title>{fields.pageTitle.value.toString() || 'Page'}</title>
+        <title>{fields?.Title?.value?.toString() || 'Page'}</title>
         <link rel="icon" href={`${publicUrl}/favicon.ico`} />
         {headLinks.map((headLink) => (
           <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
         ))}
       </Head>
 
-      <Navigation />
       {/* root placeholder for the app, which we add components to using route data */}
-      <div className="container">{route && <Placeholder name="jss-main" rendering={route} />}</div>
+      <div className={mainClassPageEditing}>
+        {layoutData.sitecore.context.renderingType === RenderingType.Component ? (
+          <DesignLibrary {...layoutData} />
+        ) : (
+          renderContent()
+        )}
+      </div>
     </>
   );
 };
