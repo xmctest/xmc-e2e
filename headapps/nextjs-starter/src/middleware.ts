8e19372d1dc48897d40dd599ad8230e9f2319c75
xmctest <xmctest@sitecore.com>
2025-11-24 20:57:52 +0800

    update 1.30.canary23

--- headapps/nextjs-starter/src/middleware.ts
+++ headapps/nextjs-starter/src/middleware.ts
@@ -18,8 +18,8 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
   // Instantiate AFTER the guard so constructors don’t run in local-only mode
   const multisite = new MultisiteMiddleware({
     /**
-    * List of sites for site resolver to work with
-    */
+     * List of sites for site resolver to work with
+     */
     sites,
     ...scConfig.api.edge,
     ...scConfig.multisite,
@@ -31,8 +31,8 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   const redirects = new RedirectsMiddleware({
     /**
-    * List of sites for site resolver to work with
-    */
+     * List of sites for site resolver to work with
+     */
     sites,
     ...scConfig.api.edge,
     ...scConfig.redirects,
@@ -45,8 +45,8 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   const personalize = new PersonalizeMiddleware({
     /**
-    * List of sites for site resolver to work with
-    */
+     * List of sites for site resolver to work with
+     */
     sites,
     ...scConfig.api.edge,
     ...scConfig.personalize,
@@ -55,6 +55,15 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
     // By default it is disabled while in development mode.
     // This is an important performance consideration since Next.js Edge middleware runs on every request
     skip: () => false,
+    // This is an example of how to provide geo data for personalization.
+    // The provided callback will be called on each request to extract geo data.
+    // extractGeoDataCb: () => {
+    //   return {
+    //     city: 'Athens',
+    //     country: 'Greece',
+    //     region: 'Attica',
+    //   };
+    // },
   });
 
   return defineMiddleware(multisite, redirects, personalize).exec(req, ev);
