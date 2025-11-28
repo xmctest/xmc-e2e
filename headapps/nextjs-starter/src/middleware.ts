a1ec358f09f26fab754cdbac432aca449d5515f1
xmctest <xmctest@sitecore.com>
2025-11-28 16:55:02 +0800

    temporary fix

--- headapps/nextjs-starter/src/middleware.ts
+++ headapps/nextjs-starter/src/middleware.ts
@@ -21,7 +21,6 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
      * List of sites for site resolver to work with
      */
     sites,
-    ...scConfig.api.edge,
     ...scConfig.multisite,
     // This function determines if the middleware should be turned off on per-request basis.
     // Certain paths are ignored by default (e.g. files and Next.js API routes), but you may wish to disable more.
@@ -35,6 +34,7 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
      */
     sites,
     ...scConfig.api.edge,
+    ...scConfig.api.local,
     ...scConfig.redirects,
     // This function determines if the middleware should be turned off on per-request basis.
     // Certain paths are ignored by default (e.g. Next.js API routes), but you may wish to disable more.
@@ -54,6 +54,8 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
     // Certain paths are ignored by default (e.g. Next.js API routes), but you may wish to disable more.
     // By default it is disabled while in development mode.
     // This is an important performance consideration since Next.js Edge middleware runs on every request
+    // NOTE: Personalize requires Edge configuration and cannot work with local containers.
+    // The middleware will disable itself if Edge config is not present.
     skip: () => false,
     // This is an example of how to provide geo data for personalization.
     // The provided callback will be called on each request to extract geo data.
