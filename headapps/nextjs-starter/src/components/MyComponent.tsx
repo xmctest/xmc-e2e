f542f869922672dfc579e5edda3b63e324141a5d
xmctest <xmctest@sitecore.com>
2026-01-19 20:57:29 +0800

    update byoc component

--- headapps/nextjs-starter/src/components/MyComponent.tsx
+++ headapps/nextjs-starter/src/components/MyComponent.tsx
@@ -1,55 +1,39 @@
-// import React from 'react';
-// import * as FEAAS from '@sitecore-feaas/clientside/react';
+import React from 'react';
+import * as FEAAS from '@sitecore-feaas/clientside/react';
 
-// interface MyComponentProps {
-//   title: string;
-//   columnsCount: number;
-// }
-
-// export const MyComponent = (props: MyComponentProps): React.JSX.Element => {
-//   const columns: string[] = [];
-//   for (let i = 0; i < props.columnsCount; i++) {
-//     columns.push(`Component Column ${i + 1}`);
-//   }
-//   return (
-//     <div className="container">
-//       <h2>{props.title || 'BYOC Demo'}</h2>
-//       <p>MyComponent Component</p>
-//       <div className="row">
-//         {columns.map((text, index) => (
-//           <div key={index} className={`col-sm-${props.columnsCount}`}>
-//             {text}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// FEAAS.External.registerComponent(MyComponent, {
-//   name: 'MyComponent',
-//   properties: {
-//     title: {
-//       type: 'string',
-//     },
-//     columnsCount: {
-//       type: 'number',
-//     },
-//   },
-// });
-import { TextField } from "@sitecore-content-sdk/nextjs";
-import { ComponentProps } from "lib/component-props";
-import { Text } from "@sitecore-content-sdk/nextjs";
-interface MyComponent extends ComponentProps {
-  fields: {
-    Quote: TextField;
-  };
+interface MyComponentProps {
+  title: string;
+  columnsCount: number;
 }
-export default function Default({ fields }: MyComponent) {
-  const { Quote } = fields;
+
+export const MyComponent = (props: MyComponentProps): React.JSX.Element => {
+  const columns: string[] = [];
+  for (let i = 0; i < props.columnsCount; i++) {
+    columns.push(`Component Column ${i + 1}`);
+  }
   return (
-    <>
-      <Text field={Quote} />
-    </>
+    <div className="container">
+      <h2>{props.title || 'BYOC Demo'}</h2>
+      <p>MyComponent Component</p>
+      <div className="row">
+        {columns.map((text, index) => (
+          <div key={index} className={`col-sm-${props.columnsCount}`}>
+            {text}
+          </div>
+        ))}
+      </div>
+    </div>
   );
-}
+};
+
+FEAAS.External.registerComponent(MyComponent, {
+  name: 'MyComponent',
+  properties: {
+    title: {
+      type: 'string',
+    },
+    columnsCount: {
+      type: 'number',
+    },
+  },
+});
