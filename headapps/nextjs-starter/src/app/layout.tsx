fa7d04c71f050ac823eaa83367de3fe3fe8c2bb1
xmctest <xmctest@sitecore.com>
2026-02-05 17:39:26 +0800

    update app router canary

--- headapps/nextjs-starter/src/app/layout.tsx
+++ headapps/nextjs-starter/src/app/layout.tsx
@@ -1,21 +1,8 @@
-import "./globals.css";
-import { Roboto } from "next/font/google";
+import './globals.css';
 
-// Configure the font
-const roboto = Roboto({
-  subsets: ["latin"], // required subset
-  weight: ["300", "400", "500", "700"], // weights you need
-  display: "swap", // ensures non-blocking text rendering
-  variable: "--font-roboto", // optional CSS variable for easier usage
-});
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={roboto.variable}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
